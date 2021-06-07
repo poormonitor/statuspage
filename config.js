989f7089e6b6eb7f43ab34a4c18ef62a62c88b44
Yifan Sun <62340684+poormonitor@users.noreply.github.com>
2021-06-07 17:25:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+window.Config = {
+  SiteName: 'Uptime Status',
+  SiteUrl: '/',
+  ApiDomain: 'mute-star-8b76.poormonitor.workers.dev',
+  ApiKeys: [
+    'ur1198157-a7cda2941b79ba658fee6bfb'
+  ],
+  ShowLink: false,
+  CountDays: 60,
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.oldmonitor.cn/'
+    }
+  ]
+};
